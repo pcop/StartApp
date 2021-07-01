3071315dd1eafc33652e5f30c5c4ae0822cc9618
鄒小城 <rachen@eri.com.tw>
2021-07-01 11:37:07 +0800

    加入專案檔案。

---
+++ StartApp/Controller/StartAppController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StartApp.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StartAppController : ControllerBase
+    {
+        // GET api/<StartAppController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            if (id == 1) {
+                Process.Start("notepad");
+            }
+            if (id == 2) {
+                Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+            }
+            return id.ToString();
+        }
+    }
+}
